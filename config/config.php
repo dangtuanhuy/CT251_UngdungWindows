f3a644903eb0e3d86c0d4056ca622e51e3ec9a90
Huy <huyb1505883@student.ctu.edu.vn>
2018-01-13 16:03:03 +0700

    Upload source

---
+++ config/config.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+    The important thing to realize is that the config file should be included in every
+    page of your project, or at least any page you want access to these settings.
+    This allows you to confidently use these settings throughout a project because
+    if something changes such as your database credentials, or a path to a specific resource,
+    you'll only need to update it here.
+*/
+ 
+$config = array(
+    "db" => array(
+        // "db1" => array(
+        //     "dbname" => "database1",
+        //     "username" => "dbUser",
+        //     "password" => "pa$$",
+        //     "host" => "localhost"
+        // ),
+        // "db2" => array(
+        //     "dbname" => "database2",
+        //     "username" => "dbUser",
+        //     "password" => "pa$$",
+        //     "host" => "localhost"
+        // )
+        "dbname" => "database1",
+        "username" => "dbUser",
+        "password" => "pa$$",
+        "host" => "localhost"    ),
+    // "urls" => array(
+    //     "baseUrl" => "http://example.com"
+    // ),
+    // "paths" => array(
+    //     "resources" => "/path/to/resources",
+    //     "images" => array(
+    //         "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
+    //         "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+    //     )
+    // )
+);
+
+/*
+    Creating constants for heavily used paths makes things a lot easier.
+    ex. require_once(LIBRARY_PATH . "Paginator.php")
+*/
+defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+     
+defined("TEMPLATES_PATH")
+    or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+ 
+defined("INCLUDES_PATH")
+    or define("INCLUDES_PATH", realpath(dirname(__FILE__) . '/includes'))
+
+// /*
+//     Error reporting.
+// */
+// ini_set("error_reporting", "true");
+// error_reporting(E_ALL|E_STRCT);
+?>
